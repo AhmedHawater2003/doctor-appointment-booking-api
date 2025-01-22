185013a2be77919688147794f7efb9b1aac514eb
Anas Khafagy <anas.khafagy@gen-c.com>
2025-01-22 13:54:00 +0200

    Add Doctor Appointment Management module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DoctorAppointmentManagementModule } from './modules/doctor-appointment-management/doctor-appointment-management.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorAppointmentManagementModule],
   controllers: [AppController],
   providers: [AppService],
 })
