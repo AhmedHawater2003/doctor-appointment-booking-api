a827ded5a9755c22561355a73aec9b95786d64ee
stap20 <abdelrahmankhaled309216@gmail.com>
2025-01-24 22:22:32 +0200

    feat: create AppointmentConfirmation module
    - Add AppointmentConfirmation module under the shared folder.
    - Integrate the module into AppModule.
    - Prepare an internal folder structure for future logic implementation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AppointmentConfirmationModule } from './appointment-confirmation/appointment-confirmation.module';
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [EventEmitterModule.forRoot(), AppointmentConfirmationModule],
   controllers: [AppController],
   providers: [AppService],
 })
