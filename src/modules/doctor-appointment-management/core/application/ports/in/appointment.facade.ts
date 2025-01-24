74c8e6f1235e156dfa93a5ef716d91113d96df0c
Anas Khafagy <anas.khafagy@gen-c.com>
2025-01-25 01:20:26 +0200

    feat(api): add AppointmentFacade and AppointmentController endpoints

---
+++ src/modules/doctor-appointment-management/core/application/ports/in/appointment.facade.ts
@@ -0,0 +1,32 @@
+// src/core/application/use-cases/appointment.facade.ts
+
+import { Inject, Injectable } from '@nestjs/common';
+import { Appointment } from '../../../domain/entities/appointment.entity';
+import { AppointmentService } from '../../appointment.service';
+
+@Injectable()
+export class AppointmentFacade {
+  constructor(
+    @Inject(AppointmentService)
+    private readonly appointmentService: AppointmentService,
+  ) {}
+
+  /**
+   * Publicly exposed method that delegates to the AppointmentService.
+   */
+  public async createAppointment(
+    slotId: string,
+    patientId: string,
+    patientName: string,
+    doctorId: string,
+    appointmentTime: Date,
+  ): Promise<Appointment> {
+    return this.appointmentService.createAppointment(
+      slotId,
+      patientId,
+      patientName,
+      doctorId,
+      appointmentTime,
+    );
+  }
+}
