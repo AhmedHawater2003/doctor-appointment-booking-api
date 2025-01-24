08b46973aafb99c61b85a4fefd01b69867f30dc2
stap20 <abdelrahmankhaled309216@gmail.com>
2025-01-24 22:28:09 +0200

    feat: - Complete implementation of the infrastructure layer. - Add event listener for "appointment booked" event. - Implement application layer with a service class to handle notification sending. - Use INotificationAPI in the notification module to manage notifications.

---
+++ src/appointment-confirmation/internal/application/AppointmentConfirmationHandler.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { AppointmentConfirmationDetails } from "./AppointmentConfirmationDetails";
+import { INotificationAPI } from "src/notification/shared/INotificationAPI";
+
+@Injectable()
+export class AppointmentConfirmationHandler{
+    constructor(@Inject(INotificationAPI) private readonly notificationApi: INotificationAPI) { }
+    
+    public handle(appointmentConfirmationDetails: AppointmentConfirmationDetails): void {
+        var notificationPayload = {
+            patientName: appointmentConfirmationDetails.patientName,
+            appointmentTime: appointmentConfirmationDetails.appointmentTime,
+            doctorName: appointmentConfirmationDetails.doctorName
+        }
+
+        this.notificationApi.notify(notificationPayload,"AppointmentConfirmation");
+    }
+}
