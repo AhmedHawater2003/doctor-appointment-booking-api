ef08d488a4acd7cde9618379dd0bd79e7f2d2744
Ahmed Hawater <hawatercoding@gmail.com>
2025-01-27 22:13:21 +0200

    fix: Add providers to appointment-confirmation.module.ts

--- src/modules/appointment-confirmation/internal/infrastructure/eventhandlers/AppointmentConfirmationEventHandler.ts
+++ src/modules/appointment-confirmation/internal/infrastructure/eventhandlers/AppointmentConfirmationEventHandler.ts
@@ -1,23 +1,30 @@
-import { Inject, Injectable,OnModuleInit } from "@nestjs/common";
-import { AppointmentBookedDto } from "src/modules/appointment-booking/shared/dtos/AppointmentBookedDto";
-import { IEventBus } from "src/Shared/IEventBus";
-import { AppointmentConfirmationHandler } from "../../application/AppointmentConfirmationHandler";
-import { AppointmentConfirmationMapper } from "../mappers/AppointmentConfirmationMapper";
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { AppointmentBookedDto } from 'src/modules/appointment-booking/shared/dtos/AppointmentBookedDto';
+import { IEventBus } from 'src/shared/IEventBus';
+import { AppointmentConfirmationHandler } from '../../application/AppointmentConfirmationHandler';
+import { AppointmentConfirmationMapper } from '../mappers/AppointmentConfirmationMapper';
 
 @Injectable()
 export class AppointmentConfirmationEventHandler implements OnModuleInit {
-    constructor(@Inject(IEventBus) private readonly eventBus: IEventBus, private readonly appointmentConfirmationHandler: AppointmentConfirmationHandler) { }
+  constructor(
+    @Inject(IEventBus) private readonly eventBus: IEventBus,
+    private readonly appointmentConfirmationHandler: AppointmentConfirmationHandler,
+  ) {}
 
-    private subscribe(): void {
-        this.eventBus.on('AppointmentBooked', this.handle.bind(this));
-    }
+  private subscribe(): void {
+    this.eventBus.on('AppointmentBooked', this.handle.bind(this));
+  }
 
-    onModuleInit(){
-        this.subscribe();
-    }
-    
-    public handle(payload: AppointmentBookedDto): void {
-        console.log('AppointmentConfirmationEventHandler: AppointmentBooked event received');
-        this.appointmentConfirmationHandler.handle(AppointmentConfirmationMapper.toAppointmentConfirmationDetails(payload));
-    }
+  onModuleInit() {
+    this.subscribe();
+  }
+
+  public handle(payload: AppointmentBookedDto): void {
+    console.log(
+      'AppointmentConfirmationEventHandler: AppointmentBooked event received',
+    );
+    this.appointmentConfirmationHandler.handle(
+      AppointmentConfirmationMapper.toAppointmentConfirmationDetails(payload),
+    );
+  }
 }
--- src/modules/appointment-confirmation/shared/appointment-confirmation.module.ts
+++ src/modules/appointment-confirmation/shared/appointment-confirmation.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { AppointmentConfirmationHandler } from '../internal/application/AppointmentConfirmationHandler';
+import { INotificationAPI } from 'src/modules/notification/shared/INotificationAPI';
+import { EmailNotification } from 'src/modules/notification/internal/EmailNotification';
+import { AppointmentConfirmationEventHandler } from '../internal/infrastructure/eventhandlers/AppointmentConfirmationEventHandler';
+import { IEventBus } from 'src/shared/IEventBus';
+import { EventBus } from 'src/shared/EventBus';
 
-@Module({})
+@Module({
+  providers: [
+    AppointmentConfirmationHandler,
+    AppointmentConfirmationEventHandler,
+    {
+      provide: INotificationAPI,
+      useClass: EmailNotification,
+    },
+    {
+      provide: IEventBus,
+      useClass: EventBus,
+    },
+  ],
+})
 export class AppointmentConfirmationModule {}
